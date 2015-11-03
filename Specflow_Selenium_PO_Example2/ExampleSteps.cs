bb9029845937fa21997b9c89c18d5288537885b7
markwinspear <markwinspear@googlemail.com>
2015-11-03 13:18:13 +0000

    first commit

---
+++ Specflow_Selenium_PO_Example2/ExampleSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace Specflow_Selenium_PO_Example2
+{
+    [Binding]
+    public class ExampleSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+           // ScenarioContext.Current.Pending();
+            Debug.WriteLine("The Given step is doing something at least");
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
