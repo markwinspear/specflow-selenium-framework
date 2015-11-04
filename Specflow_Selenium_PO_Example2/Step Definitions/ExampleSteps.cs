107082af4610336e0f2b363ae61cd089da6c3d90
markwinspear <markwinspear@googlemail.com>
2015-11-04 15:50:22 +0000

    basic implementation to get template feature to pass added references to Selenium and Selenium Support

--- Specflow_Selenium_PO_Example2/Step Definitions/ExampleSteps.cs
+++ Specflow_Selenium_PO_Example2/Step Definitions/ExampleSteps.cs
@@ -7,23 +7,27 @@ namespace Specflow_Selenium_PO_Example2
     [Binding]
     public class ExampleSteps
     {
+
+        int total = 0;
+
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int p0)
         {
-           // ScenarioContext.Current.Pending();
-            Debug.WriteLine("The Given step is doing something at least");
+            total += p0;
+            
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            ScenarioContext.Current.Pending();
+            
+
         }
         
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
-            ScenarioContext.Current.Pending();
+            Debug.WriteLine(total);
         }
     }
 }
