ab0302a916d8d0cf9651d8eaba0750823cfd1d7a
markwinspear <markwinspear@googlemail.com>
2015-11-17 09:15:16 +0000

    added pickles.bat to generate PO friendly documents

---
+++ Specflow_Selenium_PO_Example2/run pickles.bat
@@ -0,0 +1,7 @@
+cd /D E:\"Google Drive"\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2
+.\packages\Pickles.CommandLine.2.0.0\tools\pickles.exe^
+ --feature-directory=./Specflow_Selenium_PO_Example2\Features
+ --output-directory=.\documentation^
+ --test-results-format=specrun^
+ --link-results-file=.\bin\Debug\TestResult.xml
+pause
