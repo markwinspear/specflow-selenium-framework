ac1da401e7281fc9eaaf176cddd0c6438aa3b311
markwinspear <markwinspear@googlemail.com>
2015-11-17 08:56:48 +0000

    Added bat files to execute via nunit console and to generate html reports

---
+++ Specflow_Selenium_PO_Example2/Execute tests using NUnit console (Step1).bat
@@ -0,0 +1,4 @@
+cd /d E:\"Google Drive"\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2\Specflow_Selenium_PO_Example2\
+nunit-console /labels /out=TestResult.txt /xml=TestResult.xml bin\Debug\Specflow_Selenium_PO_Example2.dll
+
+pause
---
+++ Specflow_Selenium_PO_Example2/Run Specflow enhanced reporting (Step 2) .bat
@@ -0,0 +1,7 @@
+cd /d E:\"Google Drive"\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2\Specflow_Selenium_PO_Example2\
+specflow nunitexecutionreport^
+ Specflow_Selenium_PO_Example2.csproj^
+ /out:"TestResult.html"^
+ /xsltFile:"E:\Google Drive\Documents\Cucumber_Selenium_CSharp\specflow-report-templates\nunit-dream\ExecutionReport.xslt"^
+ /xmlTestResult:"E:\Google Drive\Documents\Cucumber_Selenium_CSharp\Specflow_Selenium_PO_Example2\packages\NUnit.Runners.2.6.4\tools\TestResult.xml"
+pause
